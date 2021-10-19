83016560bd6a1c058181de8f418744878ebbf5c7
Jishnudeep Bhattacharjee <bjishnudeep@gmail.com>
2021-10-19 09:53:15 +0530

    wrapped up List component and some API fixes

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -13,22 +13,10 @@ import PlaceDetails from "../PlaceDetails/PlaceDetails";
 
 import useStyles from "./styles";
 
-const List = () => {
+const List = ({ places }) => {
   const classes = useStyles();
   const [type, setType] = useState("restaurants");
   const [rating, setRating] = useState(3);
-
-  const places = [
-    { name: "CoolPlace" },
-    { name: "HOtPLace" },
-    { name: "PlacePlace" },
-    { name: "CoolPlace" },
-    { name: "HOtPLace" },
-    { name: "PlacePlace" },
-    { name: "CoolPlace" },
-    { name: "HOtPLace" },
-    { name: "PlacePlace" },
-  ];
 
   return (
     <div className={classes.container}>
